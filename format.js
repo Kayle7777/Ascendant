e9a1738f759a6cd8111302b4e0caf12911bcb292
Kayle7777 <jessewebb2@gmail.com>
2020-03-21 19:43:36 -0500

    blah

--- format.js
+++ format.js
@@ -4,10 +4,11 @@ const fs      = require('fs'),
       folder  = path.join(__dirname, '/pages-forum');
 fs.readdir(folder, (__err, data) => {
     for (let filename of data) {
-        let dir = path.join(__dirname, '/pages-forum/', filename);
-        let $   = cheerio.load(fs.readFileSync(dir));
-        $('*').prepend('<div style="color:white; background:darkgray;">');
-        $('*').append('</div>');
-        fs.writeFileSync(dir, $('*').html());
+        let dir  = path.join(__dirname, '/pages-forum/', filename);
+        let $    = cheerio.load(fs.readFileSync(dir)),
+            body = $('*');
+        body.prepend('<div style="color:white; background:darkgray;">');
+        body.append('</div>');
+        fs.writeFileSync(dir, body.html());
     }
 });
