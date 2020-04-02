c473a581d8c1fffe6a2ab49ec5dc9ce58397d516
Jesse Webb <jesse.webb@claritymis.com>
2020-04-01 23:20:49 -0500

    pages forum latest

--- fetch.js
+++ fetch.js
@@ -7,7 +7,9 @@ const fs       = require('fs'),
     { url: 'https://forums.spacebattles.com/threads/imperium-ascendant-heresy-less-40k.596194/16/reader/', pages: 3 },
     { url: 'https://forums.spacebattles.com/threads/imperium-ascendant-heresy-less-40k.596194/13/reader/', pages: 2 },
     { url: 'https://forums.spacebattles.com/threads/imperium-ascendant-heresy-less-40k.596194/19/reader/', pages: 3 },
-];
+      ],
+     baseFileDir = '/pages-forum/';
+     
 let pages = (() => {
     let arr = [
         ...baseUrls.map(obj => obj.url),
@@ -49,7 +51,7 @@ Promise.all(pages).then(
                                 post = $('div.bbWrapper', this).html();
                             // console.log(path.join(__dirname, `/pages-forum/${type}_${title}_${time}.html`))
                             fs.writeFileSync(
-                                path.join(__dirname, `/pages-edited/${time.getTime()}--${type}_${title}.html`),
+                                path.join(__dirname, `${baseFileDir}${time.getTime()}--${type}_${title}.html`),
                                 '<div style="color:white;background:#0000DF;padding: 2.5em;font-size: 2em;font-family: monospace;">' + post + '</div>'
                             );
                         });
