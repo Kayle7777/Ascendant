9f6f885ed05fbf051cef3cdd8c64f6df5a62cafb
Jesse Webb <jesse.webb@claritymis.com>
2020-04-04 16:16:14 -0500

    asc.js

--- pages-edited/asc.js
+++ pages-edited/asc.js
@@ -1,10 +1,13 @@
 $(document).keydown(function(e) {
+    console.log(e.which);
     switch(e.which) {
-        case 37: // left
-        $('.sidenav>a').click();
+        case 37: {
+            return $('.sidenav').find('a')[0].click();
+        }
 
-        case 38: // up
-        $('.sidnav-right>a').click();
+        case 39: {
+            return $('.sidenav-right').find('a')[0].click();
+        }
 
         default: return; // exit this handler for other keys
     }
