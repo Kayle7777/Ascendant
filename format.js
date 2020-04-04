4a846ad95d4e996ba7759b5556585833d92213ea
Jesse Webb <jesse.webb@claritymis.com>
2020-04-04 16:07:54 -0500

    stuff

--- format.js
+++ format.js
@@ -1,21 +1,45 @@
 const fs      = require('fs'),
       path    = require('path'),
+      cheerio = require('cheerio'),
       folder  = path.join(__dirname, '/pages-edited');
-fs.readdir(folder, (__err, filenames) => {
-    for (let filename of filenames) {
-        let absoluteFilePath = path.join(folder, filename),
-            fileText = fs.readFileSync(absoluteFilePath, { encoding: 'utf-8' })
-                         .replace(
-                             'style> <br>',
-                             '> <br>'
-                         )
-                         .replace(
-                             '<div style="color:white;background:#0000DF;padding: 2.5em;font-size: 2em;font-family: monospace;">',
-                             '<link rel="stylesheet" href="asc.css"><div class="storytext">'
-                         ); 
 
-        fs.writeFile(absoluteFilePath, fileText, 'utf8', function (err) {
-            if (err) return console.log(err);
-        });
-    }
-});
+function formatFiles(callback) {
+    fs.readdir(folder, (__err, filenames) => {
+        filenames = filenames.filter(name => !/\.js$|\.css$/.test(name));
+        for (let filename of filenames) {
+            let absoluteFilePath = path.join(folder, filename),
+                fileText = fs.readFileSync(absoluteFilePath, { encoding: 'utf-8' });
+            fs.writeFile(absoluteFilePath, callback(absoluteFilePath, fileText, filenames), 'utf8', function (err) {
+                if (err) return console.log(err);
+            });
+        }
+    });
+};
+
+const callbacks = {
+    cheerioMakeButtons: function(absoluteFilePath, fileText, filenames) {
+        // make filenames absolute path
+        filenames = filenames.map(name => path.join(folder, name));
+        let prevButtonLink = filenames[filenames.indexOf(absoluteFilePath) - 1] || filenames[filenames.length - 1],
+            nextButtonLink = filenames[filenames.indexOf(absoluteFilePath) + 1] || filenames[0];
+        $ = cheerio.load(fileText);
+        $('.storytext').prepend($(`<div class="sidenav"><a type='button' rel='noopener noreferrer' href="${prevButtonLink}"><</a></div>`));
+        $('.storytext').append($(`<div class="sidenav-right"><a type='button' rel='noopener noreferrer' href="${nextButtonLink}">></a></div>`));
+        return $.html();
+    },
+    addAscJs: function(__absoluteFilePath, fileText) {
+        return fileText.replace(
+            '<script type="text/javascript" href="asc.js"></script><link rel="stylesheet" href="asc.css">',
+            '<script type="text/javascript" src="asc.js"></script><link rel="stylesheet" href="asc.css">'
+            );
+    },
+    
+    removeStyle: function(__absoluteFilePath, fileText) {
+        return fileText.replace('style="font-size: 0.750rem"', '')
+    },
+}
+
+
+
+// formatFiles(callbacks.addAscJs);
+
--- pages-edited/asc.js
+++ pages-edited/asc.js
@@ -1,3 +1,12 @@
-$(document).ready(function() {
-    console.log($('.storytext'));
+$(document).keydown(function(e) {
+    switch(e.which) {
+        case 37: // left
+        $('.sidenav>a').click();
+
+        case 38: // up
+        $('.sidnav-right>a').click();
+
+        default: return; // exit this handler for other keys
+    }
+    e.preventDefault(); // prevent the default action (scroll / move caret)
 });
