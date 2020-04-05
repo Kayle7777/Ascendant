86b8b6a6e6b13b7bebe4f8c616ffdd777b342c28
Jesse Webb <jesse.webb@claritymis.com>
2020-04-05 18:24:27 -0500

    add actual js changes

--- fetch.js
+++ fetch.js
@@ -1,15 +1,37 @@
+'use strict';
+
+Object.defineProperty( String.prototype, 'toCheerioObject', {
+    value: function() {
+        return cheerio.load(this);
+    },
+});
+
 const fs       = require('fs'),
       path     = require('path'),
       fetch    = require('node-fetch'),
       cheerio  = require('cheerio'),
+      formatting = require('./format'),
       baseUrls = [
     { url: 'https://forums.spacebattles.com/threads/imperium-ascendant-heresy-less-40k.596194/reader/', pages: 6 },
     { url: 'https://forums.spacebattles.com/threads/imperium-ascendant-heresy-less-40k.596194/16/reader/', pages: 3 },
     { url: 'https://forums.spacebattles.com/threads/imperium-ascendant-heresy-less-40k.596194/13/reader/', pages: 2 },
     { url: 'https://forums.spacebattles.com/threads/imperium-ascendant-heresy-less-40k.596194/19/reader/', pages: 3 },
       ],
-     baseFileDir = '/pages-forum/';
-     
+     pagesForum = '/pages-forum/',
+     pipe = (...fns) => (x) => fns.reduce((v, f) => f(v), x);
+
+function buildPost(post, filePath) {
+    return pipe(
+            formatting.removeStyle,
+            formatting.addStorytextDiv,
+            formatting.addLocalCss,
+            formatting.addJQueryCDN,
+            formatting.addLocalJs,
+            formatting.addSidebarButtons(filePath),
+        )
+    (post);
+}
+
 let pages = (() => {
     let arr = [
         ...baseUrls.map(obj => obj.url),
@@ -48,11 +70,16 @@ Promise.all(pages).then(
                                     .replace(/"/g, '')
                                     .replace(/\.?New$/, ''),
                                 time = new Date($('time', this).attr('datetime')),
-                                post = $('div.bbWrapper', this).html();
-                            // console.log(path.join(__dirname, `/pages-forum/${type}_${title}_${time}.html`))
+                                fileName = `${pagesForum}${time.getTime()}--${type}_${title}.html`,
+                                filePath = path.join(__dirname, fileName),
+                                post = $('div.bbWrapper', this).html(),
+                                builtPost = buildPost(post, filePath);
+                            if (fs.existsSync(filePath) /* || forceRewrite */) {
+                                return;
+                            };
                             fs.writeFileSync(
-                                path.join(__dirname, `${baseFileDir}${time.getTime()}--${type}_${title}.html`),
-                                '<link rel="stylesheet" href="asc.css"><div class="storytext">' + post + '</div>'
+                                filePath,
+                                builtPost
                             );   
                         });
                     }
@@ -63,3 +90,4 @@ Promise.all(pages).then(
     },
     reject => console.log(reject).catch(err => console.log(err))
 );
+
--- format.js
+++ format.js
@@ -1,65 +1,54 @@
 'use strict';
 const fs      = require('fs'),
       path    = require('path'),
-      cheerio = require('cheerio'),
-      folder  = path.join(__dirname, '/pages-edited');
-
-Object.defineProperty( String.prototype, 'toCheerioObject', {
-    value: function() {
-        return cheerio.load(this);
-    },
-});
-
-function formatFiles(callback) {
-    fs.readdir(folder, (__err, filenames) => {
-        filenames = filenames.filter(name => !/\.js$|\.css$/.test(name));
-        for (let filename of filenames) {
-            let absoluteFilePath = path.join(folder, filename),
-                fileText = fs.readFileSync(absoluteFilePath, { encoding: 'utf-8' });
-            fs.writeFile(absoluteFilePath, callback(absoluteFilePath, fileText, filenames), 'utf8', function (err) {
-                if (err) return console.log(err);
-            });
-        }
-    });
-};
+      folder  = path.join(__dirname, '/pages-forum');
 
 const callbacks = {
-    cheerioMakeButtons: function(absoluteFilePath, fileText, filenames) {
+    addSidebarButtons: function(filePath) {
         // make filenames absolute path
-        filenames = filenames.map(name => path.join(folder, name));
-        let prevButtonLink = filenames[filenames.indexOf(absoluteFilePath) - 1] || filenames[filenames.length - 1],
-            nextButtonLink = filenames[filenames.indexOf(absoluteFilePath) + 1] || filenames[0];
-        $ = fileText.toCheerioObject();
-        $('.storytext').prepend($(`<div class="sidenav"><a type='button' rel='noopener noreferrer' href="${prevButtonLink}"><</a></div>`));
-        $('.storytext').append($(`<div class="sidenav-right"><a type='button' rel='noopener noreferrer' href="${nextButtonLink}">></a></div>`));
-        return $.html();
-    },
-    addAscJs: function(__absoluteFilePath, fileText) {
-        return fileText.replace(
-            '<script type="text/javascript" href="asc.js"></script><link rel="stylesheet" href="asc.css">',
-            '<script type="text/javascript" src="asc.js"></script><link rel="stylesheet" href="asc.css">'
-            );
+        const filenames = fs.readdirSync(folder)
+                .filter(name => !/\.js$|\.css$/.test(name))
+                .map(name => path.join(folder, name));
+        return fileText => {
+            let prevButtonLink = filenames[filenames.indexOf(filePath) - 1] || filenames[filenames.length - 1],
+                nextButtonLink = filenames[filenames.indexOf(filePath) + 1] || filenames[0],
+                $ = fileText.toCheerioObject();
+            $('.storytext').prepend($(`<div class="sidenav"><a type='button' rel='noopener noreferrer' href="${prevButtonLink}"></a></div>`));
+            $('.storytext').append($(`<div class="sidenav-right"><a type='button' rel='noopener noreferrer' href="${nextButtonLink}"></a></div>`));
+            return $.html()
+        };
     },
     
-    removeStyle: function(__absoluteFilePath, fileText) {
+    addStorytextDiv: function(fileText) {
+        return '<html><head></head><body><div class="storytext">' + fileText + '</div></body></html>';
+    },
+
+    removeStyle: function(fileText) {
         return fileText.replace('style="font-size: 0.750rem"', '')
     },
 
-    addJQueryCDN: function(__absoluteFilePath, fileText) {
+    addJQueryCDN: function(fileText) {
         return fileText.replace(
-            '<script type="text/javascript" src="asc.js">',
-            '<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script><script type="text/javascript" src="asc.js">'
+            '</head>',
+            '<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script></head>'
         );
     },
 
-    cheerioRemoveCarets(__absoluteFilePath, fileText) {
-        fileText = fileText.toCheerioObject();
-        fileText('a').attr('tabIndex', '0');        
-        return fileText.html();
+    addLocalJs: function(fileText) {
+        return fileText.replace(
+            '</head>',
+            '<script type="text/javascript" src="asc.js"></script></head>'
+        );
+    },
+
+    addLocalCss: function(fileText) {
+        return fileText.replace(
+            '</head>',
+            '<link rel="stylesheet" href="asc.css"></head>'
+        );
     }
 }
 
-
-
-formatFiles(callbacks.cheerioRemoveCarets);
-
+module.exports = {
+    ...callbacks    
+}
