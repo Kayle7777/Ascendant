217829bc0ac896018c8714534f07048a0c5a1901
Jesse Webb <jesse.webb@claritymis.com>
2020-04-05 23:02:33 -0500

    forgot to save format

--- format.js
+++ format.js
@@ -19,25 +19,6 @@ module.exports = {
         };
     },
 
-    getLocalFilenames: (editedFolder = false) => {
-        let _folder = __dirname + (editedFolder ? '/pages-forum/' : '/pages-edited/');
-        return fs.readdirSync(_folder)
-                 .filter(name => !/\.js$|\.css$/.test(name))
-    },
-    
-    getLocalFiles: function(editedFolder = false) {
-        let _folder = __dirname + (editedFolder ? '/pages-forum/' : '/pages-edited/');
-        return this.getLocalFilenames(editedFolder).reduce((accu, fileName, i) => {
-            i = i.toString();
-            const fullPath = path.join(_folder, fileName);
-            accu[i] = {
-                title: fileName,
-                content: fs.readFileSync(fullPath, { encoding: 'utf-8' })
-            }
-            return accu;    
-        }, {})
-    },
-
     addStorytextDiv: function(fileText) {
         return '<html><head></head><body><div class="storytext">' + fileText + '</div></body></html>';
     },
