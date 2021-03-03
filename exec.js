7300f4028dfb9ae1a66bc61560e269c3c96f8dcd
Jesse Webb <jesse.webb@claritymis.com>
2021-03-02 19:26:27 -0600

    undefined

--- exec.js
+++ exec.js
@@ -1,69 +1,67 @@
 'use strict';
 const path = require('path'),
-      fs = require('fs'),
-      cheerio = require('cheerio');
+  fs = require('fs'),
+  cheerio = require('cheerio');
 
 const { pipe } = require('./format.js');
 
-Object.defineProperty( String.prototype, 'toCheerioObject', {
-    value: function() {
-        return cheerio.load(this);
-    },
+Object.defineProperty(String.prototype, 'toCheerioObject', {
+  value: function () {
+    return cheerio.load(this);
+  },
 });
 
 const scripts = require('./scripts.js'),
-    format = require('./format.js'),
-    fetch = require('./fetch.js');
+  format = require('./format.js'),
+  fetch = require('./fetch.js');
 
 // init, find what story pieces we already have
 
-
-
 class GetPost {
-    localFiles; // [ { title: string, post: string } ]
-    scrapedPost; // type
-                 // title
-                 // time
-                 // fileName
-                 // post
-    isDuplicate; // Boolean; true if post is present on disk already
-    updateLocalFiles() {
-        // console.log(scripts.files.getLocalFiles());
-        this.localFiles = scripts.files.getLocalFiles();
-    }
-    init() {
-        // Check post name against fileName;
-        if (this.localFiles.some((lf) => lf.title.replace(/^\w+(?<=_)/, '') === this.scrapedPost.title)) {
-            return; // don't need to do anything
-        }
-        let toWrite = format.buildHtmlPipe(this.scrapedPost.post, this.scrapedPost.fileName);
-        this.writeFile(this.scrapedPost, toWrite);
-    }
+  updateLocalFiles() {
+    // console.log(scripts.files.getLocalFiles());
+    this.localFiles = scripts.files.getLocalFiles();
+  }
+  async init() {
+    // Check post name against fileName;
+    // if (this.localFiles.some((lf) => lf.title.replace(/^\w+(?<=_)/, '') === this.scrapedPost.title)) {
+    //   return; // don't need to do anything
+    // }
+    let toWrite = await format.buildHtmlPipe(this.scrapedPost.post, this.scrapedPost.fileName);
+    this.writeFile(this.scrapedPost, toWrite);
+  }
 
-    writeFile(scrapedPost, toWrite) {
-        fs.writeFileSync(path.join(__dirname, 'pages-forum', scrapedPost.fileName), toWrite);
-    }
-    
-    constructor(scrapedPost) {
-        this.updateLocalFiles();
-        this.scrapedPost = scrapedPost;
-        this.init();
-    }
+  writeFile(scrapedPost, toWrite) {
+    fs.writeFileSync(path.join(__dirname, 'pages-forum', scrapedPost.fileName), toWrite);
+  }
+
+  constructor(scrapedPost) {
+    this.localFiles = null; // [ { title: string, post: string } ]
+    this.scrapedPost = null; // type
+    // title
+    // time
+    // fileName
+    // post
+    this.isDuplicate = null; // Boolean; true if post is present on disk already
+    this.updateLocalFiles();
+    this.scrapedPost = scrapedPost;
+    this.init();
+  }
 }
 
 fetch.getJayfictionPosts((posts) => {
-    for (let i = 0; i < posts.length; i++) {
-        // gets the post and writes it
-        const post = posts[i];
-        new GetPost(post);
-        // if (i > 2) {
-        //     // console.log(posts[i]);
-        //     break;
-        // }
-    }
-    // iterate it again to do the sidebar links
-    for (let i = 0; i < posts.length; i++) {
-        // const post = posts[i];
-        // fs.writeFileSync(path.join(__dirname, 'pages-forum', post.fileName), format.addSidebarButtons(post.fileName)(post.post));
-    }
+  for (let i = 0; i < posts.length; i++) {
+    // gets the post and writes it
+    const post = posts[i];
+    new GetPost(post);
+    // if (i > 2) {
+    //     // console.log(posts[i]);
+    //     break;
+    // }
+  }
+  // iterate it again to do the sidebar links
+  for (let i = 0; i < posts.length; i++) {
+    // const post = posts[i];
+    // fs.writeFileSync(path.join(__dirname, 'pages-forum', post.fileName), format.addSidebarButtons(post.fileName)(post.post));
+  }
 });
--- format.js
+++ format.js
@@ -1,87 +1,88 @@
 'use strict';
-const fs      = require('fs'),
-      path    = require('path'),
-      scripts = require('./scripts.js');
+const fs = require('fs'),
+  path = require('path'),
+  scripts = require('./scripts.js');
 
-module.exports = {    
-    pipe: (...fns) => (x) => fns.reduce((v, f) => f(v), x),
-    buildHtmlPipe: (post, fileName) => {
-        return module.exports.pipe(
-                module.exports.removeStyle,
-                module.exports.addStorytextDiv,
-                module.exports.addLocalCss,
-                module.exports.addJQueryCDN,
-                module.exports.addLocalJs,
-            )(post);
-    },
+module.exports = {
+  pipe: (...fns) => (x) => fns.reduce((v, f) => f(v), x),
+  buildHtmlPipe: async (post, fileName) => {
+    return module.exports.pipe(
+      module.exports.removeStyle,
+      module.exports.addStorytextDiv,
+      module.exports.addLocalCss,
+      module.exports.addJQueryCDN,
+      module.exports.addLocalJs,
+      await module.exports.addSidebarButtons(post)
+    )(post);
+  },
 
-    removeStyle: function(fileText) {
-        return fileText.replace('style="font-size: 0.750rem"', '')
-    },
+  removeStyle: function (fileText) {
+    return fileText.replace('style="font-size: 0.750rem"', '');
+  },
 
-    addStorytextDiv: function(fileText) {
-        return '<html><head></head><body><div class="storytext">' + fileText + '</div></body></html>';
-    },
-    
-    addLocalCss: function(fileText) {
-        return fileText.replace(
-            '</head>',
-            '<link rel="stylesheet" href="asc.css"></head>'
+  addStorytextDiv: function (fileText) {
+    return '<html><head></head><body><div class="storytext">' + fileText + '</div></body></html>';
+  },
+
+  addLocalCss: function (fileText) {
+    return fileText.replace('</head>', '<link rel="stylesheet" href="asc.css"></head>');
+  },
+
+  addJQueryCDN: function (fileText) {
+    return fileText.replace(
+      '</head>',
+      '<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script></head>'
+    );
+  },
+
+  addLocalJs: function (fileText) {
+    return fileText.replace('</head>', '<script type="text/javascript" src="asc.js"></script></head>');
+  },
+
+  addSidebarButtons: (fileName) => {
+    // Get absolute path
+    //   .map(name => path.join(__dirname, 'pages-forum', name));
+    return (fileText) => {
+      new Promise((resolve, reject) => {
+        let $ = fileText.toCheerioObject(),
+          filenames = scripts.files.getLocalFilenames(),
+          prevButtonLink = filenames[filenames.indexOf(fileName) - 1] || filenames[filenames.length - 1],
+          nextButtonLink = filenames[filenames.indexOf(fileName) + 1] || filenames[0];
+        console.log(prevButtonLink);
+        console.log(nextButtonLink);
+        $('.storytext').prepend(
+          $(`<div class="sidenav"><a type='button' rel='noopener noreferrer' href="${prevButtonLink}"></a></div>`)
         );
-    },
-
-    addJQueryCDN: function(fileText) {
-        return fileText.replace(
-            '</head>',
-            '<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script></head>'
+        $('.storytext').append(
+          $(`<div class="sidenav-right"><a type='button' rel='noopener noreferrer' href="${nextButtonLink}"></a></div>`)
         );
-    },
+        return resolve($.html());
+      });
+    };
+  },
 
-    addLocalJs: function(fileText) {
-        return fileText.replace(
-            '</head>',
-            '<script type="text/javascript" src="asc.js"></script></head>'
-        );
-    },
-
-    addSidebarButtons: (fileName) => {
-        // Get absolute path
-        const filenames = scripts.files.getLocalFilenames();
-                        //   .map(name => path.join(__dirname, 'pages-forum', name));
-        return fileText => {
-            let              $ = fileText.toCheerioObject(),
-                prevButtonLink = filenames[filenames.indexOf(fileName) - 1] || filenames[filenames.length - 1],
-                nextButtonLink = filenames[filenames.indexOf(fileName) + 1] || filenames[0];
-            console.log(prevButtonLink);
-            console.log(nextButtonLink);
-            $('.storytext').prepend($(`<div class="sidenav"><a type='button' rel='noopener noreferrer' href="${prevButtonLink}"></a></div>`));
-            $('.storytext').append($(`<div class="sidenav-right"><a type='button' rel='noopener noreferrer' href="${nextButtonLink}"></a></div>`));
-            return $.html();
-        };
-    },
-
-    scrapePageForJayfictionPosts: function(page) {
-        const $ = page.toCheerioObject();
-        const articles = [];
-        $('article.hasThreadmark[data-author="Jayfiction"]').each(function() {
-            let time = new Date($('time', this).attr('datetime')),
-                type = $('.message-cell--threadmark-header label', this)
-                        .text()
-                        .replace(/[\\\/:]/g, '-')
-                        .replace(/"/g, ''),
-                title = $('.message-cell--threadmark-header span', this)
-                        .text()
-                        .replace(/[\\\/:]/g, '-')
-                        .replace(/"/g, '')
-                        .replace(/New$/, '')
-            return articles.push({
-                type: type,
-                title: title,
-                time: time,
-                fileName: `${time.getTime()}--${type}_${title}.html`,
-                post: $('div.bbWrapper', this).html()
-            });
-        });
-        return articles;
-    },
-}
+  scrapePageForJayfictionPosts: function (page) {
+    const $ = page.toCheerioObject();
+    const articles = [];
+    $('article.hasThreadmark[data-author="Jayfiction"]').each(function () {
+      let time = new Date($('time', this).attr('datetime')),
+        type = $('.message-cell--threadmark-header label', this)
+          .text()
+          .replace(/[\\\/:]/g, '-')
+          .replace(/"/g, ''),
+        title = $('.message-cell--threadmark-header span', this)
+          .text()
+          .replace(/[\\\/:]/g, '-')
+          .replace(/"/g, '')
+          .replace(/New$/, '');
+      return articles.push({
+        type: type,
+        title: title,
+        time: time,
+        fileName: `${time.getTime()}--${type}_${title}.html`,
+        post: $('div.bbWrapper', this).html(),
+      });
+    });
+    return articles;
+  },
+};
